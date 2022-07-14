ff255d365f1b8e8f870c4ac8ddc00fbf328d1319
Michael Okello <okellofrost@gmail.com>
2022-07-14 12:42:09 +0300

    using bind and the rest

--- index.js
+++ index.js
@@ -79,10 +79,131 @@ const person = {
       return `${person.firstName}`+' '+`${person.lastName}`;
     },
     set fullName(value){
+        // Error Handling
+        if (typeof value !== 'string') 
+
+        throw new Error('Undefined : Value is Not A String');
+
         const parts = value.split(' ');
+        //Error Handling
+        if(parts.length !== 2)
+            throw new Error('Enter A First & Last Name');
+
+
         this.firstName = parts[0];
         this.lastName = parts[1];
     }
 };
-person.fullName = 'Kevin Samuels';
+
+try {
+    person.fullName = 'John Smith';
+} catch (error) {
+    //sconsole.log(error);
+    alert(error);
+}
+
 console.log(person);
+
+//This keyword
+// This references the object that is referencing  the current function
+ const video = {
+    title: 'My Title',
+    play(){
+        console.log(this);
+    }
+ };
+
+ //Lets add a stop method to the video object
+
+
+ video.stop = function(){
+    console.log(this);
+ }
+
+ video.stop();
+ //Second example with this as a global object
+ function playVideo(){
+
+    console.log(this);
+
+}
+
+playVideo();
+
+// The same using a constructor function
+
+function Video (title){
+    this.title = title;
+    console.log(this);
+}
+
+const vidz = new Video('Jack Beanstalk');
+//another example  with more objects in video 
+// This references the object that is referencing  the current function
+// const video2 = {
+//     title: 'My Title',
+//     tags:['Tag a','Tag b','Tag c','Tag d'],
+
+//     showTags(){
+//       this.tags.forEach(function(tag ){
+//         console.log(this.title,tag);
+//       },this);
+//     }
+//  };
+
+// const video2 = {
+//     title: 'Bind My Title',
+//     tags:['Tag a','Tag b','Tag c','Tag d'],
+
+//     showTags(){
+//       this.tags.forEach(function(tag ){
+//         console.log(this.title,tag);
+//       }.bind(this));
+//     }
+//  };
+
+//this is the most preferred way of doing this
+
+//Arrow functions inherit the This parameter
+const video2 = {
+    title: ' My Title',
+    tags:['Tag a','Tag b','Tag c','Tag d'],
+
+    showTags(){
+      this.tags.forEach( tag =>{
+        console.log(this.title,tag);
+      });
+    }
+ };
+  video2.showTags();
+//SECOND SOLUTION FOR CHANGING THIS
+// This references the object that is referencing  the current function
+//  const video2 = {
+//         title: 'My Title',
+//         tags:['Tag a','Tag b','Tag c','Tag d'],
+    
+//         showTags(){
+
+//         const self = this ;//this will reference the video object
+
+//           this.tags.forEach(function(tag ){
+//             console.log(self.title,tag);
+//           },this);
+//         }
+//      };
+
+//Dont use the approach bovSe
+
+// //changin this
+// function playVideo2(a , b){
+//     console.log('Play Video 2',this);
+// }
+
+// playVideo2.call({name: 'Okello Call'}, 1,2);
+// playVideo2.apply({name: 'Okello Apply'}, [1,2]);//another way but this takes second arg as an array
+
+// //Store result in an object
+// const fn = playVideo2.bind({name : 'Okello Bind'});
+// fn();
+
+// //playVideo2();
